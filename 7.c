670996ba1362b9a384c602647301daab337cca59
Ciana Lim <Ciana Lim>
2015-12-01 17:04:03 +0800

    changed algo

--- 7.c
+++ 7.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-	int n, temp, i, j, arr[1000], k=0, ix, jx;
-	printf("Problem 7: Given a number n, all the fractions less than 1, such that n is the denominator, is printed (in lowest terms).\n")
+	int n, temp, i, j, arr[1000], k=0, ix, jx, tempo[1000], tempor;
+	printf("Problem 7: Given a number n, all the fractions less than 1, such that n is the denominator, is printed (in lowest terms).\n");
 	printf("Enter n: ");
 	scanf("%d", &n);
 	for(i=2; i<100000; i++)
@@ -22,15 +22,22 @@ int main(){
 		}
 	}
 	temp=k;
+	tempor=2;
+	for(i=0; i<n-2; i++){
+		tempo[i]=tempor;
+		tempor++;
+	}
 	printf("The output is: 1/%d", n);
-	for(ix=2; ix<n; ix++){
-		for(k=0; k<=temp; k++){
-			if(ix%arr[k]==0){
-				ix++;
+	for(i=0; i<n-2; i++){
+		for(k=0; k<temp; k++){
+			if(tempo[i]%arr[k]==0){
 				break;
 			}
+			if(k+1==temp){
+				printf(", %d/%d", tempo[i], n);
+			}
+			
 		}
-		printf(", %d/%d", ix, n);
 	}
 		
 }
