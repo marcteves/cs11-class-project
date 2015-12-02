97ed5c683e6a786feb7167c7bdc84ce1df8e94a7
Ciana Lim <Ciana Lim>
2015-12-02 17:51:46 +0800

    edited function names

--- 14.c
+++ 14.c
@@ -2,13 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct node{
+struct node14{
 	int data;
-	struct node *next_node;
+	struct node14 *next_node;
 };
 
-struct node* add_num(struct node *head, struct node *current, int rem){
-	current=malloc(sizeof(struct node));
+struct node14* add_num14(struct node14 *head, struct node14 *current, int rem){
+	current=malloc(sizeof(struct node14));
 	current->data=rem;
 	current->next_node=head;
 	head=current;
@@ -16,16 +16,16 @@ struct node* add_num(struct node *head, struct node *current, int rem){
 }
 
 int main(){
-	struct node *head=NULL, *current;
+	struct node14 *head=NULL, *current;
 	int base, n, rem;
 	printf("Enter number: ");
 	scanf("%d", &n);
 	printf("Enter base: ");
 	scanf("%d", &base);
-	printf("The output is: ");
+	
 	while(n!=0){
 		rem=n%base;
-		head=add_num(head, current, rem);
+		head=add_num14(head, current, rem);
 		n=n/base;
 	}
 	current=head;
--- 15.c
+++ 15.c
@@ -2,35 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct node{
+struct node15{
 	int num;
-	struct node *next_node;
+	struct node15 *next_node;
 };
 
-struct node* create_list(struct node *head, struct node *current, char temp[]){
-	current=malloc(sizeof(struct node));
+struct node15* create_list15(struct node15 *head, struct node15 *current, char temp[]){
+	current=malloc(sizeof(struct node15));
 	current->num=atoi(temp);
 	current->next_node=head;
 	head=current;
 	return head;
 }
 
-struct node* add_num(struct node *head, struct node *current, char temp[]){
-	current=malloc(sizeof(struct node));
+struct node15* add_num15(struct node15 *head, struct node15 *current, char temp[]){
+	current=malloc(sizeof(struct node15));
 	current->num=atoi(temp);
 	current->next_node=head;
 	head=current;
 	return head;
 }
 
-void print_list(struct node *current){
+void print_list15(struct node15 *current){
 	while(current){
 		printf("%d\n", current->num);
 		current=current->next_node;
 	}
 }
 
-struct node* delete(struct node *c, struct node *head){
+struct node15* delete15(struct node15 *c, struct node15 *head){
 	if(c->num==head->num){
 			c=c->next_node;
 			head=c;
@@ -38,18 +38,18 @@ struct node* delete(struct node *c, struct node *head){
 		}
 		else{
 			if(c->next_node==NULL){
-				struct node *temp1=head->next_node;
-				struct node *temp2=head;
+				struct node15 *temp1=head->next_node;
+				struct node15 *temp2=head;
 				while(temp1->next_node){
 					temp1=temp1->next_node;
 					temp2=temp2->next_node;
 				}
-				temp2->next_node=(struct node*)NULL;
+				temp2->next_node=(struct node15*)NULL;
 				free(temp1);
 				return head;
 			}
 			else{
-				struct node *prev=head;
+				struct node15 *prev=head;
 				while(prev->next_node!=c){
 					prev=prev->next_node;
 				}
@@ -60,8 +60,8 @@ struct node* delete(struct node *c, struct node *head){
 		}
 }
 
-struct node* getLowest(struct node *head, struct node *current){
-	struct node *c=current;
+struct node15* getLowest15(struct node15 *head, struct node15 *current){
+	struct node15 *c=current;
 	static int i=0;
 	int currentLowest=current->num;
 		while(current->next_node){
@@ -78,13 +78,13 @@ struct node* getLowest(struct node *head, struct node *current){
 		else{
 			printf(", %d", c->num);
 		}
-		return delete(c, head);
+		return delete15(c, head);
 }
 
 int main(){
 	int n, i;
 	char string[1000], temp[1000]="";
-	struct node *head=NULL, *current;
+	struct node15 *head=NULL, *current;
 	printf("Input n: ");
 	scanf("%d", &n);
 	getchar();
@@ -97,16 +97,16 @@ int main(){
 			strncat(temp, &string[i], 1);
 		}
 		if(head==NULL){
-			head=create_list(head, current, temp);
+			head=create_list15(head, current, temp);
 			memset(temp, 0, sizeof(temp));
 		}
 		else{
-			head=add_num(head, current, temp);
+			head=add_num15(head, current, temp);
 			memset(temp, 0, sizeof(temp));
 		}
 	}
 	while(head){
 		current=head;
-		head=getLowest(head, current);
+		head=getLowest15(head, current);
 	}
 }
